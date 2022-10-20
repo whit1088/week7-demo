101d66f855c68747be8d6852ee6cf9247ed2c0e2
Jade <whit1088@algonquinlive.com>
2022-10-19 20:14:58 -0400

    hi

--- src/components/Card.js
+++ src/components/Card.js
@@ -2,6 +2,8 @@ import React from "react";
 import Grid from "@mui/material/Grid";
 import Paper from "@mui/material/Paper";
 import Typography from "@mui/material/Typography";
+import Rating from "@mui/material/Rating";
+
 
 function Card({data}) {
   if (data.length === 0) {
@@ -11,14 +13,17 @@ function Card({data}) {
       <Grid container spacing={2}>
         {data.map((item) => (
           <Grid item xs={12} sm={6} md={4} key={item.id}>
-            <Paper elevation={3} className="paper">
-              <img src={item.image} alt={item.title} className="img"/>
-              <Typography variant="body1" gutterBottom>{item.title}</Typography>
+            <Paper elevation={3} className='paper'>
+              <img src={item.image} alt={item.title} className='img' />
+              <Rating name='disabled' value={item.rating.rate} disabled />
+              <Typography variant='body1' gutterBottom>
+                {item.title}
+              </Typography>
             </Paper>
           </Grid>
         ))}
       </Grid>
-    )
+    );
   }
 }
 
